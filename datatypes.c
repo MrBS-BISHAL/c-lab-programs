3c11aa889b862298f5a9600fb20e2badfc7c38f0
MrBS-BISHAL <166684122+MrBS-BISHAL@users.noreply.github.com>
2024-05-08 12:19:56 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,17 @@
+//Write a C program to demonstrate the use of different data types (int, float, char,double). 
+#include <stdio.h>
+#include <conio.h>
+void main()
+{ 
+    int i = 45;
+    float f = 2.88;
+    char c = 'B';
+    double d = 28.888;
+
+    printf("Integer variable: %d\n", i);
+    printf("Float variable: %f\n", f);
+    printf("Character variable: %c\n", c);
+    printf("Double variable: %lf\n", d);
+
+    getch();
+}
