312eb05e89f6889f130aa918b86d61822335cc6c
MrBS-BISHAL <166684122+MrBS-BISHAL@users.noreply.github.com>
2024-05-08 12:13:13 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,19 @@
+//Develop a program to check whether a number is even or odd.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%d is even",n);
+    }
+    else
+    {
+        printf("%d is odd",n);
+    }
+
+    getch();
+}
